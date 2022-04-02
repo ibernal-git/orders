a818c16cd096b95c5d532761af542002c56ddd6d
Imanol Bernal <info@ibernal.dev>
2022-04-02 10:48:13 +0200

    Link to provide a way to change locale

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useI18N } from '../ context/i18n'
 
 const Home: NextPage = () => {
   const { t } = useI18N()
-  console.log(t('HELLO_WORLD'))
+  const { locale, locales } = useRouter()
+  const restOfLocales = locales?.filter(l => l !== locale) || []
   return (
     <>
-    <h1 className='text-lg font-bold'>{t('HELLO_WORLD')}</h1>
+      <Link href='/' locale={restOfLocales[0]}>
+        <a>{restOfLocales[0]}</a>
+      </Link>
+      <h1 className='text-lg font-bold'>{t('HELLO_WORLD')}</h1>
     </>
   )
 }
