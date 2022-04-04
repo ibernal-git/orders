58151d681b8f473e119f2d6c7aa179c5dc1b0c9c
Imanol Bernal <info@ibernal.dev>
2022-04-04 18:17:09 +0200

    new design of login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useI18N } from '../ context/i18n'
-import { signIn, getSession, getProviders } from 'next-auth/react'
+import { getSession, getProviders } from 'next-auth/react'
+import LoginButton from '../components/LoginButton'
 
 const Home: NextPage = () => {
   const { t } = useI18N()
@@ -11,15 +12,23 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Link href='/' locale={restOfLocales[0]}>
-        <a>{restOfLocales[0]}</a>
-      </Link>
-      <h1 className='text-lg font-bold'>{t('HELLO_WORLD')}</h1>
-      <div>
-        <button onClick={() => signIn('cognito', { callbackUrl: '/app/' })}>Iniciar Sesión con Cognito</button>
-      </div>
-      <div>
-        <button onClick={() => signIn('azure-ad', { callbackUrl: '/app/' })}>Iniciar Sesión con Azure</button>
+      <div className='flex h-screen items-center justify-center space-between bg-secondary'>
+        <div className='h-96 mx-auto w-96 bg-primary p-10 rounded text-center shadow-md'>
+          <h1 className='text-2xl font-bold'>{t('APP_NAME').toUpperCase()}</h1>
+          <div className='flex flex-col justify-center h-full'>
+            <div>
+              <LoginButton provider='cognito'>
+                {t('SIGN_IN', 'Cognito')}
+              </LoginButton>
+              <LoginButton provider='azure-ad'>
+                {t('SIGN_IN', 'Azure AD')}
+              </LoginButton>
+            </div>
+            <Link href='/' locale={restOfLocales[0]}>
+              <a>{t('CHANGE_LANGUAGE')} {restOfLocales[0]}</a>
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   )
