324738a659ff1644d2055265e34921e72f4dcdaa
Imanol Bernal <info@ibernal.dev>
2022-04-05 20:15:02 +0200

    protect api route with next auth getSession

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 
 type Data = {
   name: string
 }
 
-export default function handler (
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default async function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
+  const session = await getSession({ req })
+  if (!session) {
+    // Not Signed in
+    return res.status(401).end()
+  }
   res.status(200).json({ name: 'John Doe' })
+  res.end()
 }
